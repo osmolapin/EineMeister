33a84414b8e6bec9d89c6bcc9038aeb68fcdae44
Osmo Lapin <oslapi@taltech.ee>
2025-10-21 13:14:47 +0300

    added simple way of registering account in firebase

---
+++ scripts/form.js
@@ -0,0 +1,27 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAP-nzuF31UZbgHyc5AGsxgrNCVC1jb9hk",
+  authDomain: "einemeister-84e8c.firebaseapp.com",
+  projectId: "einemeister-84e8c",
+  storageBucket: "einemeister-84e8c.firebasestorage.app",
+  messagingSenderId: "699963249863",
+  appId: "1:699963249863:web:392a4b3bbab29450d9dae2",
+  measurementId: "G-5YW3F3X62G"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+// Signup function
+function signUp() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  const promise = auth.createUserWithEmailAndPassword(
+    email.value,
+    password.value
+  );
+  promise.catch((e) => alert(e.message));
+}
