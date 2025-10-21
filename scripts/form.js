6782c5704e979a6a70c5c2b80d0665d9327d35b6
Osmo Lapin <oslapi@taltech.ee>
2025-10-21 16:43:56 +0300

    Added confirm password check and send user to login page after successful register

--- scripts/form.js
+++ scripts/form.js
@@ -18,8 +18,24 @@ const auth = firebase.auth();
 function signUp() {
   var email = document.getElementById("email");
   var password = document.getElementById("password");
+  var cpassword = document.getElementById("cpassword");
+
+  if (password.value !== cpassword.value) {
+    alert("Paroolid ei kattu");
+    return;
+  }
 
   const queryAuth = auth.createUserWithEmailAndPassword(email.value, password.value);
-  queryAuth.catch((e) => alert(e.message));
-  alert("Kasutaja lisatud andmebaasi")
+  
+  queryAuth.then(() => {
+        // If authentication succeeded send user to login page
+        alert("Kasutaja lisatud andmebaasi"); 
+        window.location.href = "login.html"; 
+        
+    })
+    .catch((e) => {
+        // If failed alert user
+        alert(e.message);
+    });
+  
 }
