475767e6a908d280f801c21f86cf7f60e8125c64
oslapi <oslapi@taltech.ee>
2025-11-06 21:29:53 +0200

    Changed some styling

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -36,29 +36,41 @@ function createRecipeCard(recipe) {
     const extraInfo = document.createElement("div");
     extraInfo.classList.add("recipe-extra-info"); 
 
-    // Helper function to create a nutritional info paragraph
-    const createNutrientParagraph = (className, value, unit, title) => {
-        const p = document.createElement("p");
-        p.classList.add(className);
-        p.textContent = `${title} ${value} ${unit}`;
-        return p;
-    };
+    // // Helper function to create a nutritional info paragraph
+    // const createNutrientParagraph = (className, value, unit, title) => {
+    //     const p = document.createElement("p");
+    //     p.classList.add(className);
+    //     p.textContent = `${title} ${value} ${unit}`;
+    //     return p;
+    // };
 
-    const calories = createNutrientParagraph("calories", recipe.data.calories, "kcal", "Kalorid");
-    const carbs = createNutrientParagraph("carbs", recipe.data.carbs, "g", "Süsivesikud");
-    const proteins = createNutrientParagraph("proteins", recipe.data.proteins, "g", "Valgud");
-    const fats = createNutrientParagraph("fats", recipe.data.fats, "g", "Rasvad");
+    const macroNumbers = document.createElement("div");
+    macroNumbers.classList.add("recipe-macro-numbers");
+    macroNumbers.textContent = recipe.data.calories + " " + recipe.data.carbs + " " + recipe.data.proteins + " " + recipe.data.fats;
+
+
+    const macroNames = document.createElement("div");
+    macroNames.classList.add("recipe-macro-names");
+    macroNames.textContent = "Kalorid Süsivesikud Valgud Rasvad"
+    // const calories = createNutrientParagraph("calories", recipe.data.calories, "kcal", "Kalorid");
+    // const carbs = createNutrientParagraph("carbs", recipe.data.carbs, "g", "Süsivesikud");
+    // const proteins = createNutrientParagraph("proteins", recipe.data.proteins, "g", "Valgud");
+    // const fats = createNutrientParagraph("fats", recipe.data.fats, "g", "Rasvad");
     
-    extraInfo.appendChild(calories);
-    extraInfo.appendChild(carbs);
-    extraInfo.appendChild(proteins);
-    extraInfo.appendChild(fats);
+    // extraInfo.appendChild(calories);
+    // extraInfo.appendChild(carbs);
+    // extraInfo.appendChild(proteins);
+    // extraInfo.appendChild(fats);
+    // extraInfo.appendChild(macroNumbers);
+    // extraInfo.appendChild(macroNames);
 
     // Assemble the Card
     card.appendChild(image);
     card.appendChild(title);
     card.appendChild(price);
     card.appendChild(extraInfo);
+    card.appendChild(macroNumbers);
+    card.appendChild(macroNames);
     
     return card;
 }
