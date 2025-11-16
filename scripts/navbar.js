afc3b34a096129e14dd4cd2db3e3b4a4632c6750
Henry Hanst <hhanst@taltech.ee>
2025-11-16 22:03:24 +0200

    Saved-carts page with style. doesnt show users saved carts yet.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -363,8 +363,8 @@ class navbarComponent extends HTMLElement {
                             </a>
                             
                             <ul class="dropdown-menu" id="user-dropdown-menu">
-                                <li><a href="#">Lisa retsept</a></li>
-                                <li><a href="#">Minu ostukorvid</a></li>
+                                <li><a href="#">Minu retseptid</a></li>
+                                <li><a href="/pages/saved-carts.html">Minu ostukorvid</a></li>
                                 <li><a href="#" id="logout-link">Logi välja</a></li>
                             </ul>
                         </li>
