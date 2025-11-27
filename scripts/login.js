23c4d4347b2f34c09eddad0189597b72907617c2
Osmo Lapin <oslapi@taltech.ee>
2025-11-27 10:53:40 +0200

    Added persistance mode

--- scripts/login.js
+++ scripts/login.js
@@ -1,26 +1,37 @@
+const auth = firebase.auth();
 
-// Function is started after successful login
 function handleLoginSuccess() {
     window.location.href = '/index.html';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
-    
+    const rememberCheckbox = document.getElementById("remember");
+
     if (loginForm) {
         loginForm.addEventListener('submit', (e) => {
             e.preventDefault(); 
 
-            const emailOrUsername = loginForm.elements['email'].value;
+            const email = loginForm.elements['email'].value;
             const password = loginForm.elements['password'].value;
 
-            firebase.auth().signInWithEmailAndPassword(emailOrUsername, password)
-            .then(() => {
-                handleLoginSuccess();
-            })
-            .catch((error) => {
-                alert("Sisselogimine ebaõnnestus: " + error.message);
-            });
+            const persistenceMode = firebase.auth.Auth.Persistence.SESSION
+            if (rememberCheckbox.checked) {
+                persistenceMode = firebase.auth.Auth.Persistence.LOCAL
+            }
+
+            // Set persistence
+            auth.setPersistence(persistenceMode)
+                .then(() => {
+                    return auth.signInWithEmailAndPassword(email, password);
+                })
+                .then(() => {
+                    handleLoginSuccess();
+                })
+                .catch((error) => {
+                    console.error(error);
+                    alert("Sisselogimine ebaõnnestus: " + error.message);
+                });
         });
     }
 });
